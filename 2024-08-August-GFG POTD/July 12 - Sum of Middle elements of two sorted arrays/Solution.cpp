e8ab046e34bd7cd618e183884d700b514e1030f8
Ashishkumar448 <165567553+Ashishkumar448@users.noreply.github.com>
2024-08-12 10:36:57 +0530

    Create Solution.cpp

---
+++ 2024-08-August-GFG POTD/July 12 - Sum of Middle elements of two sorted arrays/Solution.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int SumofMiddleElements(std::vector<int>& A, std::vector<int>& B) {
+        int n = A.size();
+        std::vector<int> merged(2 * n);
+        int i = 0, j = 0, k = 0;
+
+        // Merge the two arrays
+        while (i < n && j < n) {
+            if (A[i] <= B[j]) {
+                merged[k++] = A[i++];
+            } else {
+                merged[k++] = B[j++];
+            }
+        }
+
+        // If there are remaining elements in A
+        while (i < n) {
+            merged[k++] = A[i++];
+        }
+
+        // If there are remaining elements in B
+        while (j < n) {
+            merged[k++] = B[j++];
+        }
+
+        // Find the middle elements and return their sum
+        int middle1 = merged[n - 1]; // First middle element
+        int middle2 = merged[n];     // Second middle element
+
+        return middle1 + middle2;
+    }
+};
