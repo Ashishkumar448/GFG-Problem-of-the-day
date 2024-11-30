8aa1f2efde247e0b144db24c1b3fb9be0a73b9ac
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2024-11-30 13:14:17 +0530

    Create Solution.java

---
+++ 2024-11-November-GFG-POTD/November 30 - Anagram/Solution.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+import java.util.stream.*;
+
+class GFG {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String s1 = br.readLine(); // first string
+            String s2 = br.readLine(); // second string
+
+            Solution obj = new Solution();
+
+            if (obj.areAnagrams(s1, s2)) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function is to check whether two strings are anagram of each other or not.
+    public static boolean areAnagrams(String s1, String s2) {
+
+        // Your code here
+        // If lengths are different, they can't be anagrams
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        // Frequency array for counting character occurrences (a-z)
+        int[] count = new int[26];
+
+        // Count characters in s1 and s2
+        for (int i = 0; i < s1.length(); i++) {
+            count[s1.charAt(i) - 'a']++;  // Increment count for s1 character
+            count[s2.charAt(i) - 'a']--;  // Decrement count for s2 character
+        }
+
+        // If any element is non-zero, the strings are not anagrams
+        for (int c : count) {
+            if (c != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
