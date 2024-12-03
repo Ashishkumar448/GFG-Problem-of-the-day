6d1204f883a10c6028a5a7621ef1605e1f550384
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2024-12-03 06:59:59 +0530

    Create Solution.java

---
+++ 2024-12-December-GFG-POTD/December 03 - Min Chars to Add for Palindrome/Solution.java
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    public static int minChar(String s) {
+        // Write your code here
+        // Concatenate original string, $, and reversed string
+        String concatenated = s + "$" + new StringBuilder(s).reverse().toString();
+        int n = concatenated.length();
+
+        // Build LPS array
+        int[] lps = new int[n];
+        int len = 0;
+        for (int i = 1; i < n; ) {
+            if (concatenated.charAt(i) == concatenated.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        
+        // Length of the longest palindromic suffix in original string
+        int longestPalindromicSuffix = lps[n - 1];
+        
+        // Minimum characters to add at the front
+        return s.length() - longestPalindromicSuffix;
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        var sc = new FastReader();
+        int test = sc.nextInt();
+        while (test-- > 0) {
+            String s = sc.nextString();
+            System.out.println(Solution.minChar(s));
+
+            System.out.println("~");
+        }
+    }
+
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+            st = new StringTokenizer("");
+        }
+
+        private void read() throws IOException {
+            st = new StringTokenizer(br.readLine());
+        }
+
+        public String nextString() throws IOException {
+            while (!st.hasMoreTokens()) read();
+            return st.nextToken();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(nextString());
+        }
+
+        public long nextLong() throws IOException {
+            return Long.parseLong(nextString());
+        }
+    }
+}
+// } Driver Code Ends
