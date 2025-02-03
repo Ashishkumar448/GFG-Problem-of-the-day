7348b410e76591ab0986e7f3f3a33159e2735689
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2025-02-03 13:36:35 +0530

    Create Solution.cpp

---
+++ 2025-02-February-GFG-POTD/February 03 - Height of Binary Tree/Solution.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+class Solution {
+public:
+    // Function to find the height of a binary tree.
+    int height(Node* node) {
+        return node == NULL ? -1 : 1 + max(height(node->left), height(node->right));
+    }
+};
+
+// Function to Build Tree from a string input
+Node* buildTree(string str) {
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    vector<string> ip;
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    Node* root = new Node(stoi(ip[0]));
+    queue<Node*> queue;
+    queue.push(root);
+
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+        Node* currNode = queue.front();
+        queue.pop();
+
+        string currVal = ip[i];
+
+        if (currVal != "N") {
+            currNode->left = new Node(stoi(currVal));
+            queue.push(currNode->left);
+        }
+
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        if (currVal != "N") {
+            currNode->right = new Node(stoi(currVal));
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string treeString;
+        getline(cin, treeString);
+        Node* root = buildTree(treeString);
+        Solution ob;
+        cout << ob.height(root) << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
