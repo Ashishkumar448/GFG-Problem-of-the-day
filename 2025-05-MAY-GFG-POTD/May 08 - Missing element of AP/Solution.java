a167c89e27253264fb07c836571ade108926f266
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2025-05-08 19:22:20 +0530

    Create Solution.java

---
+++ 2025-05-MAY-GFG-POTD/May 08 - Missing element of AP/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findMissing(int[] arr) {
+        int n = arr.length;
+        int d = (arr[n - 1] - arr[0]) / n;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i + 1] - arr[i] != d) {
+                return arr[i] + d;
+            }
+        }
+
+        // If no missing element, return the next element in AP
+        return arr[n - 1] + d;
+    }
+}
