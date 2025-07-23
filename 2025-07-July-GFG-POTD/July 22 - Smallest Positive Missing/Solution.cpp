64511e294066127118d5d6be8613e0a09779904a
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2025-07-23 14:26:36 +0530

    Create Solution.cpp

---
+++ 2025-07-July-GFG-POTD/July 22 - Smallest Positive Missing/Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& arr) {
+        int n = arr.size();
+
+        for (int i = 0; i < n; ++i) {
+            while (arr[i] > 0 && arr[i] <= n && arr[arr[i] - 1] != arr[i]) {
+                int correctIndex = arr[i] - 1;
+                swap(arr[i], arr[correctIndex]);
+            }
+        }
+
+        for (int i = 0; i < n; ++i) {
+            if (arr[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+};
