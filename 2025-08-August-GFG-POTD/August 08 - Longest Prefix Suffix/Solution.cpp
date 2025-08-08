f6e9eb71fb5b49ba9b98ff46887e5435b8d816e4
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2025-08-08 08:13:36 +0530

    Create Solution.cpp

---
+++ 2025-08-August-GFG-POTD/August 08 - Longest Prefix Suffix/Solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int getLPSLength(string s) {
+        int n = s.length();
+        vector<int> lps(n, 0);
+        int len = 0; // length of the previous longest prefix suffix
+        int i = 1;
+
+        while (i < n) {
+            if (s[i] == s[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1]; // try the previous prefix
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        return lps[n - 1]; // longest proper prefix-suffix length
+    }
+};
