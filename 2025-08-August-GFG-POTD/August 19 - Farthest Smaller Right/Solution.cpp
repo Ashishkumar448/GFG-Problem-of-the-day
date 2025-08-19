f1354b6a8e7d13c8d780e44bed2eabe387050266
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2025-08-19 23:15:30 +0530

    Create Solution.cpp

---
+++ 2025-08-August-GFG-POTD/August 19 - Farthest Smaller Right/Solution.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> farMin(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> ans;
+        vector<int> suff(n);
+        
+        suff[n-1] = arr[n-1];
+        for(int i = n-2; i >= 0; --i) {
+            suff[i] = min(arr[i], suff[i+1]);
+        }
+        
+        for(int i = 0; i < n; ++i) {
+            int lo = i+1, hi = n-1, res = -1;
+            while(lo <= hi) {
+                int mid = lo + (hi-lo)/2;
+                if(suff[mid] < arr[i]) {
+                    res = mid;
+                    lo = mid+1;
+                } else {
+                    hi = mid-1;
+                }
+            }
+            ans.push_back(res);
+        }
+        return ans;
+    }
+};
