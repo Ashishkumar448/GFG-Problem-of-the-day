492d8395bba4b18473f3b02f1f8a72c246a89322
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2025-09-30 11:39:59 +0530

    Create Solution.java

---
+++ 2025-09-September-GFG-POTD/September 30 - Generate all binary strings/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public ArrayList<String> binstr(int n) {
+        ArrayList<String> result = new ArrayList<>();
+        int total = 1 << n; // 2^n
+        for (int i = 0; i < total; i++) {
+            String binary = Integer.toBinaryString(i);
+            // Pad with leading zeros to make length = n
+            while (binary.length() < n) {
+                binary = "0" + binary;
+            }
+            result.add(binary);
+        }
+        return result;
+    }
+}
