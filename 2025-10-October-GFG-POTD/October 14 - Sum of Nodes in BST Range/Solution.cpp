31323d4f451d62facde6a31c94aed584c9deff2e
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2025-10-14 09:38:27 +0530

    Create Solution.cpp

---
+++ 2025-10-October-GFG-POTD/October 14 - Sum of Nodes in BST Range/Solution.cpp
@@ -0,0 +1,30 @@
+/*
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+public:
+    int nodeSum(Node* root, int l, int r) {
+        if (root == nullptr)
+            return 0;
+        
+        // If root's value is smaller than l, skip left subtree
+        if (root->data < l)
+            return nodeSum(root->right, l, r);
+        
+        // If root's value is greater than r, skip right subtree
+        if (root->data > r)
+            return nodeSum(root->left, l, r);
+        
+        // Root's value is within [l, r]
+        return root->data + nodeSum(root->left, l, r) + nodeSum(root->right, l, r);
+    }
+};
