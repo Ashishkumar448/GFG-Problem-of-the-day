8df70c261e122e7fc53b432ff4ed9e46221cb54e
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2025-11-05 11:14:53 +0530

    Create Solution.py

---
+++ 2025-11-November-GFG-POTD/November 05 - Get Minimum Squares/Solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minSquares(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        
+        for i in range(1, n + 1):
+            dp[i] = i  # worst case: all 1^2
+            j = 1
+            while j * j <= i:
+                dp[i] = min(dp[i], 1 + dp[i - j * j])
+                j += 1
+                
+        return dp[n]
