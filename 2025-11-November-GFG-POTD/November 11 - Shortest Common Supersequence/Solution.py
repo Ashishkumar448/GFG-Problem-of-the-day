c71e802100ead53201c8ae7fd9f396c899b42bf2
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2025-11-11 12:12:31 +0530

    Create Solution.py

---
+++ 2025-11-November-GFG-POTD/November 11 - Shortest Common Supersequence/Solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    @staticmethod
+    def minSuperSeq(s1: str, s2: str) -> int:
+        n, m = len(s1), len(s2)
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+        
+        # Build LCS table
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        
+        lcs = dp[n][m]
+        return n + m - lcs
+
