09f42976731aec73cfed770c0afabfc56874ca29
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2025-12-22 09:36:38 +0530

    Implement function to find row with max 1s

---
+++ 2025-12-December-GFG-POTD/December 22 - Row with max 1s/Solution.cpp
@@ -0,0 +1,22 @@
+// User function Template for C++
+
+class Solution {
+public:
+    int rowWithMax1s(vector<vector<int>> &arr) {
+        int n = arr.size();
+        int m = arr[0].size();
+
+        int rowIndex = -1;
+        int j = m - 1; // start from top-right corner
+
+        for (int i = 0; i < n; i++) {
+            while (j >= 0 && arr[i][j] == 1) {
+                rowIndex = i; // update row index
+                j--;          // move left
+            }
+        }
+
+        return rowIndex;
+    }
+};
+
