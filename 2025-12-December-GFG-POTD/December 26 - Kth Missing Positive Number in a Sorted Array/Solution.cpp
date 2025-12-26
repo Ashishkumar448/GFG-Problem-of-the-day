529ea0ce1da9a9746b0552893fc241bde33f9510
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2025-12-26 09:41:21 +0530

    Create Solution.cpp

---
+++ 2025-12-December-GFG-POTD/December 26 - Kth Missing Positive Number in a Sorted Array/Solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int kthMissing(vector<int>& arr, int k) {
+        int low = 0, high = arr.size() - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            // Count of missing numbers till index mid
+            int missing = arr[mid] - (mid + 1);
+            
+            if (missing < k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        // kth missing number
+        return low + k;
+    }
+};
