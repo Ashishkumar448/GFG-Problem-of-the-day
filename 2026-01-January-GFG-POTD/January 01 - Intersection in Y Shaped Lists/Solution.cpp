c89d72351cb58f26654f50bda0a64f3c201ff9e9
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2026-01-01 08:35:24 +0530

    Create Solution.cpp

---
+++ 2026-01-January-GFG-POTD/January 01 - Intersection in Y Shaped Lists/Solution.cpp
@@ -0,0 +1,28 @@
+/*
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int d) {
+        data = d;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* intersectPoint(Node* head1, Node* head2) {
+        Node* p1 = head1;
+        Node* p2 = head2;
+
+        // Traverse until both pointers meet
+        while (p1 != p2) {
+            p1 = (p1 == NULL) ? head2 : p1->next;
+            p2 = (p2 == NULL) ? head1 : p2->next;
+        }
+
+        // p1 (or p2) is the intersection node
+        return p1;
+    }
+};
