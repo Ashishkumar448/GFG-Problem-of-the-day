e47becf9123c2c37388f864b90c993b444b8b006
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2026-01-01 08:34:34 +0530

    Create Solution.java

---
+++ 2026-01-January-GFG-POTD/January 01 - Intersection in Y Shaped Lists/Solution.java
@@ -0,0 +1,27 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public Node intersectPoint(Node head1, Node head2) {
+        Node p1 = head1;
+        Node p2 = head2;
+
+        // Traverse until both pointers meet
+        while (p1 != p2) {
+            p1 = (p1 == null) ? head2 : p1.next;
+            p2 = (p2 == null) ? head1 : p2.next;
+        }
+
+        // p1 (or p2) is the intersection node
+        return p1;
+    }
+}
