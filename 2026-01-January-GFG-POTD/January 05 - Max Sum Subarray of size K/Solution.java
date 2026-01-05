06441c581f25b8d5e0fca1dbb03be36b46464063
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2026-01-05 18:59:28 +0530

    Create Solution.java

---
+++ 2026-01-January-GFG-POTD/January 05 - Max Sum Subarray of size K/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+     public int maxSubarraySum(int[] arr, int k) {
+        int n = arr.length;
+        
+        // Sum of first window of size k
+        int windowSum = 0;
+        for (int i = 0; i < k; i++) {
+            windowSum += arr[i];
+        }
+        
+        int maxSum = windowSum;
+        
+        // Slide the window
+        for (int i = k; i < n; i++) {
+            windowSum += arr[i] - arr[i - k];
+            maxSum = Math.max(maxSum, windowSum);
+        }
+        
+        return maxSum;
+    }
+}
