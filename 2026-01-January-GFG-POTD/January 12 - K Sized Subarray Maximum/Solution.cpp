54356d01846622b3b095280260c615e074172bea
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2026-01-12 15:07:42 +0530

    Create Solution.cpp

---
+++ 2026-01-January-GFG-POTD/January 12 - K Sized Subarray Maximum/Solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> maxOfSubarrays(vector<int>& arr, int k) {
+        vector<int> result;
+        deque<int> dq;
+
+        for (int i = 0; i < arr.size(); i++) {
+
+            // Remove indices out of current window
+            while (!dq.empty() && dq.front() <= i - k)
+                dq.pop_front();
+
+            // Remove smaller elements
+            while (!dq.empty() && arr[dq.back()] <= arr[i])
+                dq.pop_back();
+
+            // Add current index
+            dq.push_back(i);
+
+            // Store result from k-1 onward
+            if (i >= k - 1)
+                result.push_back(arr[dq.front()]);
+        }
+        return result;
+    }
+};
