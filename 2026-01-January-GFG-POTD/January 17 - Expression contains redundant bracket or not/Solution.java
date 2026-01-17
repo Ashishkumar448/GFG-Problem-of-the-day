b13d989075b7582f0e45a265b6c24bd296835cc1
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2026-01-17 14:23:44 +0530

    Create Solution.java

---
+++ 2026-01-January-GFG-POTD/January 17 - Expression contains redundant bracket or not/Solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public static boolean checkRedundancy(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == ')') {
+                boolean hasOperator = false;
+
+                // Check contents inside parentheses
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    char top = stack.pop();
+                    if (top == '+' || top == '-' || top == '*' || top == '/') {
+                        hasOperator = true;
+                    }
+                }
+
+                // Remove opening '('
+                stack.pop();
+
+                // No operator found → redundant brackets
+                if (!hasOperator) {
+                    return true;
+                }
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        return false;
+    }
+}
