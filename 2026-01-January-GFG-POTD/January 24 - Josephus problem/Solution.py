3f679ad6dff3ca9e26c120a665601667285f0d09
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2026-01-24 22:36:57 +0530

    Create Solution.py

---
+++ 2026-01-January-GFG-POTD/January 24 - Josephus problem/Solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def josephus(self, n, k):
+        res = 0  # 0-based index
+        
+        for i in range(2, n + 1):
+            res = (res + k) % i
+        
+        return res + 1  # convert to 1-based index
