3a1c11b0cabe1bde46cee9f97a43380725798411
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2026-02-20 17:13:32 +0530

    Create Solution.py

---
+++ 2026-02-February-GFG-POTD/February 20 - Form the Largest Number/Solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findLargest(self, arr):
+        # Convert integers to strings
+        nums = list(map(str, arr))
+        
+        # Sort using custom comparator
+        nums.sort(key=lambda x: x*10, reverse=True)
+        
+        # Edge case: if largest element is "0"
+        if nums[0] == "0":
+            return "0"
+        
+        # Join and return result
+        return "".join(nums)
