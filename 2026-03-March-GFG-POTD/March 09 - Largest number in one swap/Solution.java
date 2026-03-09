e85cad97fc4e4fef171d35f4cbdfcadf6c558f05
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2026-03-09 09:19:03 +0530

    Create Solution.java

---
+++ 2026-03-March-GFG-POTD/March 09 - Largest number in one swap/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String largestSwap(String s) {
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+
+        // maxIdx[i] = index of the largest digit from i to end
+        int[] maxIdx = new int[n];
+        maxIdx[n - 1] = n - 1;
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] > arr[maxIdx[i + 1]]) {
+                maxIdx[i] = i;
+            } else {
+                maxIdx[i] = maxIdx[i + 1];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < arr[maxIdx[i]]) {
+                char temp = arr[i];
+                arr[i] = arr[maxIdx[i]];
+                arr[maxIdx[i]] = temp;
+                break;
+            }
+        }
+
+        return new String(arr);
+    }
+}
