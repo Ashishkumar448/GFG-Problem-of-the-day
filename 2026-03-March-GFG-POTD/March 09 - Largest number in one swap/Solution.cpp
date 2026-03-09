474ea4ac809e84d1e1b5d1e17550c569dafd029f
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2026-03-09 09:19:57 +0530

    Create Solution.cpp

---
+++ 2026-03-March-GFG-POTD/March 09 - Largest number in one swap/Solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string largestSwap(string s) {
+        int n = s.length();
+        vector<int> maxIdx(n);
+
+        maxIdx[n - 1] = n - 1;
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (s[i] > s[maxIdx[i + 1]])
+                maxIdx[i] = i;
+            else
+                maxIdx[i] = maxIdx[i + 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] < s[maxIdx[i]]) {
+                swap(s[i], s[maxIdx[i]]);
+                break;
+            }
+        }
+
+        return s;
+    }
+};
