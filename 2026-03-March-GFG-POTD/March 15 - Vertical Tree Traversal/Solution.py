703c801fb3e8b279d5ca6352f686e03ccca4b45e
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2026-03-15 09:17:26 +0530

    Create Solution.py

---
+++ 2026-03-March-GFG-POTD/March 15 - Vertical Tree Traversal/Solution.py
@@ -0,0 +1,26 @@
+from collections import deque, defaultdict
+
+class Solution:
+    def verticalOrder(self, root):
+        if not root:
+            return []
+        
+        mp = defaultdict(list)
+        q = deque([(root, 0)])
+        
+        while q:
+            node, hd = q.popleft()
+            
+            mp[hd].append(node.data)
+            
+            if node.left:
+                q.append((node.left, hd - 1))
+            
+            if node.right:
+                q.append((node.right, hd + 1))
+        
+        result = []
+        for key in sorted(mp.keys()):
+            result.append(mp[key])
+        
+        return result
