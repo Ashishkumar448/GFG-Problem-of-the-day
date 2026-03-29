6eec13d91a826cc3631867907e8990195d45ae67
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2026-03-29 18:27:49 +0530

    Create Solution.py

---
+++ 2026-03-March-GFG-POTD/March 29 - Partitions with Given Difference/Solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countPartitions(self, arr, diff):
+        total = sum(arr)
+
+        # Check validity
+        if (total + diff) % 2 != 0 or total < diff:
+            return 0
+
+        target = (total + diff) // 2
+
+        dp = [0] * (target + 1)
+        dp[0] = 1
+
+        for num in arr:
+            for j in range(target, num - 1, -1):
+                dp[j] += dp[j - num]
+
+        return dp[target]
