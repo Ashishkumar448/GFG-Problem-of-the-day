a52678b0aac98b34bbea3b251445d101ccf1fe7f
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2026-04-18 09:06:11 +0530

    Create Solution.py

---
+++ 2026-04-April-GFG-POTD/April 18 - Flip to Maximize 1s/Solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxOnes(self, arr):
+        total_ones = sum(arr)
+        
+        max_gain = 0
+        curr_gain = 0
+        
+        for x in arr:
+            # Transform: 0 -> +1, 1 -> -1
+            val = 1 if x == 0 else -1
+            
+            curr_gain = max(val, curr_gain + val)
+            max_gain = max(max_gain, curr_gain)
+        
+        return total_ones + max_gain
