e7eb38301c40008bbb48521e711802acd5e7ed4c
Ashish kumar <165567553+Ashishkumar448@users.noreply.github.com>
2026-04-19 15:28:03 +0530

    Create solution.java

---
+++ 2026-04-April-GFG-POTD/April 19 - Check for Power/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+     public boolean isPower(int x, int y) {
+        // Edge case
+        if (x == 1) return y == 1;
+
+        // Keep dividing y by x if possible
+        while (y % x == 0) {
+            y /= x;
+        }
+
+        // If we reach 1, it's a power of x
+        return y == 1;
+    }
+}
